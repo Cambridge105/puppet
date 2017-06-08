66adf639f9f1756db5e1fc9b009248cf7c503310
rmc47 <robert@syxis.co.uk>
2017-06-08 22:56:32 +0100

    Starting work on a Rivendell module

---
+++ modules/role-rivendell-client/manifests/init.pp
@@ -0,0 +1,18 @@
+class role-rivendell-client {
+  package { 'git':
+    ensure => latest,
+  }
+  package { 'curl':
+    ensure => latest,
+  }
+
+  file { '/rdbuild':
+    ensure => directory,
+  }
+
+  exec { 'Download Rivendell source':
+    creates => '/rdbuild/rivendell-2.16.0.tar.gz',
+    exec => '/usr/bin/curl https://github.com/ElvishArtisan/rivendell/archive/v2.16.0.tar.gz > /rdbuild/rivendell-2.16.0.tar.gz',
+    require => File['/rdbuild'],
+  }
+}
