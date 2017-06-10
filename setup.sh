3870d5c83bc153d8fb0405b956383eca86eb7e71
Robert Chipperfield <robert.chipperfield@red-gate.com>
2017-06-10 10:54:50 +0100

    Fix package names for Puppet 4, and fully-qualify puppet executable

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
-sudo dpkg -i puppetlabs-release-wheezy.deb
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-`lsb_release -cs`.deb || exit 1
+sudo dpkg -i puppetlabs-release-pc1-`lsb_release -cs`.deb
 sudo apt-get update
-sudo apt-get install puppet
-sudo puppet module install puppetlabs-vcsrepo
-sudo puppet module install puppetlabs-inifile
-sudo puppet apply manifests/site.pp --modulepath=modules/:/etc/puppet/modules/
+sudo apt-get install puppet-agent
+sudo /opt/puppetlabs/bin/puppet module install puppetlabs-vcsrepo
+sudo /opt/puppetlabs/bin/puppet module install puppetlabs-inifile
+sudo /opt/puppetlabs/bin/puppet apply manifests/site.pp --modulepath=modules/:/etc/puppet/modules/
