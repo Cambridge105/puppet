168350a8bd8f9f69537fc96c678a176f5b63eedc
Robert Chipperfield <robert.chipperfield@red-gate.com>
2017-06-10 10:54:29 +0100

    All the various packages needed for building Rivendell

--- modules/role_rivendell_client/manifests/packages.pp
+++ modules/role_rivendell_client/manifests/packages.pp
@@ -3,21 +3,82 @@ class role_rivendell_client::packages {
     ensure => latest,
   }
   package { 'wget':
-    ensure => latest,
+    ensure => installed,
   }
   package { 'vim':
+    ensure => installed,
+  }
+  package { 'openssh-server':
+    ensure => installed,
+  }
+
+  package { 'mariadb-server':
     ensure => latest,
   }
-  package {'autoconf':
+  package { 'mariadb-client':
     ensure => latest,
+  }
+
+  package { 'libqt4-sql-mysql':
+    ensure => installed,
+  }
+
+  # All the stuff we need for building Rivendell...
+  package {'autoconf':
+    ensure => installed,
   }
   package {'build-essential':
-    ensure => latest,
+    ensure => installed,
   }
   package {'libtool':
-    ensure => latest,
+    ensure => installed,
   }
   package { 'libqt4-dev':
+    ensure => installed,
+  }
+  package { 'libsamplerate-dev':
+    ensure => installed,
+  }
+  package { 'libsndfile-dev':
+    ensure => installed,
+  }
+  package { 'libcdparanoia-dev':
+    ensure => installed,
+  }
+  package { 'libid3-dev':
+    ensure => installed,
+  }
+  package { 'libcurl4-openssl-dev':
     ensure => latest,
+  }
+  package { 'libpam-dev':
+    ensure => installed,
+  }
+  package { 'libsoundtouch-dev':
+    ensure => installed,
+  }
+  package { 'libmad0-dev':
+    ensure => installed,
+  }
+  package { 'libasound-dev':
+    ensure => installed,
+  }
+  package { 'libmp4v2-dev':
+    ensure => installed,
+  }
+  package { 'libfaad-dev':
+    ensure => installed,
+  }
+  package { 'libflac++-dev':
+    ensure => installed,
+  }
+  package { 'xsltproc':
+    ensure => installed,
+  }
+  package {'docbook-xsl':
+    ensure => installed,
+  }
+  package { 'fop':
+    ensure => installed,
   }
 }
