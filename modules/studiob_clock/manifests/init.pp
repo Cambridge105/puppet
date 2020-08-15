7d63ab3591bea18d71206c80454b8e5fcb1f529e
Robert Chipperfield <robert@syxis.co.uk>
2020-08-15 15:46:05 +0100

    Studio B HDMI display settings

--- modules/studiob_clock/manifests/init.pp
+++ modules/studiob_clock/manifests/init.pp
@@ -3,4 +3,28 @@ class studiob_clock {
   class { 'info_display::kiosk_browser':
     url => 'http://localhost/index.html?studio=b',
   }
+
+  file_line {'HDMI hotplug':
+    line  => 'hdmi_force_hotplug=1',
+    path  => '/boot/config.txt',
+    match => '.*hdmi_force_hotplug.*', # /.*match/
+  }
+
+  file_line {'HDMI group':
+    line  => 'hdmi_group=2',
+    path  => '/boot/config.txt',
+    match => '.*hdmi_group.*', # /.*match/
+  }
+
+  file_line {'HDMI mode':
+    line  => 'hdmi_mode=87',
+    path  => '/boot/config.txt',
+    match => '.*hdmi_mode.*', # /.*match/
+  }
+
+  file_line {'HDMI CVT':
+    line  => 'hdmi_cvt 1024 600 60 6 0 0 0',
+    path  => '/boot/config.txt',
+    match => '.*hdmi_cvt.*', # /.*match/
+  }
 }
