425c577ab13f4c2d8d7edf8529a77a87c931353f
Robert Chipperfield <robert@syxis.co.uk>
2020-08-15 14:51:51 +0100

    Rename info_display to meet module naming convention

---
+++ modules/info_display/manifests/init.pp
@@ -0,0 +1,80 @@
+class info_display {
+  package { 'chromium':
+    ensure => latest,
+  }
+
+  package {'vim':
+    ensure => present,
+  }
+
+  package { 'ntp':
+    ensure => present,
+  }
+
+  package { 'git':
+    ensure => present,
+  }
+
+  service {'ntp':
+    ensure  => running,
+    require => Package['ntp'],
+  }
+
+  package { 'python':
+    ensure => present,
+  }
+
+  package { 'python-pip':
+    ensure => present,
+  }
+
+  package { 'web.py':
+    ensure   => present,
+    provider => pip,
+    require  => Package['python-pip'],
+  }
+
+  service { 'ssh':
+    ensure => running,
+    enable => true,
+  }
+
+  file { '/home/pi/.config/lxsession/LXDE-pi':
+    ensure => directory,
+    owner  => 'pi',
+    group  => 'pi',
+  }
+  file { '/etc/timezone':
+    ensure  => present,
+    content => "Europe/London\n",
+  }
+  file { '/etc/localtime':
+    ensure => link,
+    target => '/usr/share/zoneinfo/Europe/London',
+  }
+
+  vcsrepo { '/home/pi/studio-screen':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/Cambridge105/studio-screen.git',
+    require  => [ Package['git'] ],
+    user     => 'pi',
+    revision => 'master',
+  }
+
+  ini_setting { 'xserver-command':
+    ensure  => present,
+    path    => '/etc/lightdm/lightdm.conf',
+    section => 'SeatDefaults',
+    setting => 'xserver-command',
+    value   => 'X -s 0 dpms',
+  }
+
+  cron { 'update-schedule':
+    ensure  => present,
+    command => 'wget -O /home/pi/studio-screen/schedule.js http://cambridge105.fm/schedule_info/get_schedule_entries_min.php',
+    user    => 'pi',
+    hour    => [0, 12, 21],
+    minute  => [15],
+  }
+}
