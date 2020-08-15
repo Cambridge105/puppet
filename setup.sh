9cd1a4290f11125e3150df0b163e9b703311b57e
Robert Chipperfield <robert@syxis.co.uk>
2020-08-15 14:52:13 +0100

    Install puppet6 from gem rather than apt

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "Usage: sudo bash setup.sh" 1>&2
+   exit 1
+fi
+
 # Make sure we have a sensible hostname
 if [ -z "$NEWHOSTNAME" ]; then
     read -p "Enter a hostname for this machine: " NEWHOSTNAME
@@ -7,14 +14,31 @@ echo "Using new hostname from NEWHOSTNAME: $NEWHOSTNAME"
 hostname $NEWHOSTNAME
 echo $NEWHOSTNAME > /etc/hostname
 
-RELEASE_NAME=`lsb_release -c -s`
-wget https://apt.puppetlabs.com/puppet6-release-${RELEASE_NAME}.deb || exit 1
-dpkg -i puppet6-release-${RELEASE_NAME}.deb || exit 1
-apt-get update || exit 1
-apt-get install puppet-agent || exit 1
+# Puppet don't ship a nice package for ARM, but we can do it by installing the Ruby gem instead...
+apt update
+apt install ruby-full -y
+gem install puppet --version '~> 6' --no-rdoc --no-ri
 
-# Note: we used to add /opt/puppetlabs/bin to the path here, but it would seem Puppet 5.5 at least puts it in /usr/bin/puppet anyway, so no need...
+# These modules are normally baked into the agent install, but since we aren't using the
+# official packages on ARM, install them manually...
+# https://github.com/puppetlabs/puppet-specifications/blob/master/moving_modules.md
+puppet module install puppetlabs-augeas_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-cron_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-host_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-k5login_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-mailalias_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-maillist_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-mount_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-nagios_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-selinux_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-sshkeys_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-yumrepo_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-zfs_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
+puppet module install puppetlabs-zone_core --target-dir /opt/puppetlabs/puppet/vendor_modules/
 
-sudo /opt/puppetlabs/bin/puppet module install puppetlabs-vcsrepo
-sudo /opt/puppetlabs/bin/puppet module install puppetlabs-inifile
-sudo /opt/puppetlabs/bin/puppet apply manifests/site.pp --modulepath=modules/:/etc/puppet/modules/:/etc/puppetlabs/code/environments/production/modules/
+# Extra modules we require...
+puppet module install puppetlabs-vcsrepo
+puppet module install puppetlabs-inifile
+
+# And finally, the first puppet run!
+puppet apply manifests/site.pp --modulepath=modules/:/etc/puppet/modules/:/etc/puppetlabs/code/environments/production/modules/
