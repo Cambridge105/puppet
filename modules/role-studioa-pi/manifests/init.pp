aaafde06e478b6418f07db7b0b32d93c9862c200
Robert Chipperfield <robert@syxis.co.uk>
2020-08-15 15:07:49 +0100

    Use the kiosk browser module

--- modules/role-studioa-pi/manifests/init.pp
+++ modules/role-studioa-pi/manifests/init.pp
@@ -1,20 +1,6 @@
 class role-studioa-pi {
-  require info-display
-  
-  file { '/etc/xdg/lxsession/LXDE-pi/autostart':
-    source => 'puppet:///modules/role-studioa-pi/autostart',
-    ensure => present,
-    owner => root,
-    group => root,
-  }
-
-  file { '/home/pi/.config/lxsession/LXDE/autostart':
-    source => 'puppet:///modules/role-studioa-pi/autostart',
-    ensure => present,
-    owner => pi,
-    group => pi,
-  }
-
+  require info_display
+  require info_display::kiosk_browser
 
   cron { 'pollmiclive':
     command => '/usr/bin/python /home/pi/studio-screen/pollMicLive.py 8081',
