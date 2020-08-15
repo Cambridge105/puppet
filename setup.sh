0a0b842c584d7cf36a762188ee183d05f176e7ac
Robert Chipperfield <robert@syxis.co.uk>
2020-08-15 14:27:32 +0100

    Update Puppet script to use the official builds for all architectures, and upgrade to Puppet 6

--- setup.sh
+++ setup.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
-if (grep -q -i arm /proc/cpuinfo); then
-	# No ARM release of recent Puppet agent, so need to compile it ourselves...
-	echo "Beginning set up. This compiles from source so will likely take a long time..."
-	# add puppet repo
-	mkdir setup-temp
-	cd setup-temp
-
-	apt-get update || exit 1
-	apt-get install ruby-full facter hiera unzip -y || exit 1
-	gem install bundler
-
-	wget https://github.com/puppetlabs/puppet/archive/5.5.10.tar.gz || exit 1
-	tar xzf 5.5.10.tar.gz || exit 1
-	cd puppet-5.5.10
-
-	bundle install --path .bundle/gems || exit 1
-	bundle update || exit 1
-	ruby install.rb || exit 1
-else
-	# Use the official builds of the agent for x86 / x64
-	RELEASE_NAME=`lsb_release -c -s`
-	wget https://apt.puppetlabs.com/puppet5-release-${RELEASE_NAME}.deb || exit 1
-    dpkg -i puppet5-release-${RELEASE_NAME}.deb || exit 1
-    apt-get update || exit 1
-	apt-get install puppet-agent || exit 1
-fi
+# Use the official builds of the agent for x86 / x64
+RELEASE_NAME=`lsb_release -c -s`
+wget https://apt.puppetlabs.com/puppet6-release-${RELEASE_NAME}.deb || exit 1
+dpkg -i puppet6-release-${RELEASE_NAME}.deb || exit 1
+apt-get update || exit 1
+apt-get install puppet-agent || exit 1
 
 # Note: we used to add /opt/puppetlabs/bin to the path here, but it would seem Puppet 5.5 at least puts it in /usr/bin/puppet anyway, so no need...
 
